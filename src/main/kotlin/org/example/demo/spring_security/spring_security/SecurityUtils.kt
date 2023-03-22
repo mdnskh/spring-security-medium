a73446b8b09e0734b9f68fbb956a0114651fea4f
Naimuddin Shek <naimuddin.shek@trendyol.com>
2023-03-22 16:28:31 +0100

    Remove enviroment logic from security utils

--- src/main/kotlin/org/example/demo/spring_security/spring_security/SecurityUtils.kt
+++ src/main/kotlin/org/example/demo/spring_security/spring_security/SecurityUtils.kt
@@ -1,19 +1,13 @@
 package org.example.demo.spring_security.spring_security
 
-import org.springframework.core.env.Environment
-import org.springframework.security.core.authority.SimpleGrantedAuthority
 import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.security.core.userdetails.User
 import org.springframework.stereotype.Component
 
 @Component
-class SecurityUtils(private val environment: Environment) {
+class SecurityUtils {
 
     fun getCurrentUser(): User {
-        if (environment.activeProfiles.contains("dev")
-            && !environment.activeProfiles.contains("live")) {
-            return User("testUserName", "testPassword", listOf(SimpleGrantedAuthority("USER")))
-        }
         return SecurityContextHolder.getContext().authentication.details as User
     }
 }
--- src/test/kotlin/org/example/demo/spring_security/spring_security/HelloIntegrationTest.kt
+++ src/test/kotlin/org/example/demo/spring_security/spring_security/HelloIntegrationTest.kt
@@ -1,23 +1,37 @@
 package org.example.demo.spring_security.spring_security
 
+import io.mockk.every
+import io.mockk.mockk
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.context.TestConfiguration
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Import
+import org.springframework.context.annotation.Primary
+import org.springframework.security.core.userdetails.User
 import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.get
 
 @ActiveProfiles("dev")
-@AutoConfigureMockMvc
 @SpringBootTest
+@AutoConfigureMockMvc
+@Import(TestConfig::class)
 class HelloIntegrationTest {
 
     @Autowired
-    private lateinit var mockMvc:MockMvc
+    private lateinit var securityUtils: SecurityUtils
+
+    @Autowired
+    private lateinit var mockMvc: MockMvc
 
     @Test
     fun sayHello(){
+        val user: User = mockk()
+        every { securityUtils.getCurrentUser() } returns user
+        every { user.username } returns "testUserName"
 
         mockMvc.get("/")
             .andDo { print() }
@@ -27,5 +41,14 @@ class HelloIntegrationTest {
                 jsonPath("$.text") {  value("Hello testUserName! Welcome to spring security.") }
             }
     }
+}
 
+
+@TestConfiguration
+class TestConfig {
+    @Bean
+    @Primary
+    fun securityUtil() : SecurityUtils {
+        return mockk()
+    }
 }
